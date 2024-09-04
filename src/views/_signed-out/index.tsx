d538fdda4b0e6e6bc45ab436f22e12870de6f9f9
Mary <git@mary.my.id>
2024-09-04 20:49:55 +0700

    fix: change description

--- src/views/_signed-out/index.tsx
+++ src/views/_signed-out/index.tsx
@@ -10,8 +10,16 @@ const SignedOutPage = () => {
 				<p class="text-2xl font-medium">posteing</p>
 				<p class="mt-1 text-sm">lets you send bluesky threads/multiple posts at once</p>
 				<p class="mt-6 text-pretty text-sm text-contrast-muted">
-					aka. i'm willing to give support to my thread poster thing but i sure as hell don't want to maintain
-					yet another bluesky web client
+					aka. holy shit i'm burned from maintaining a bluesky web client but bluesky team still hasn't got
+					around to taking over my{' '}
+					<a
+						target="_blank"
+						href="https://github.com/bluesky-social/social-app/pull/4163"
+						class="text-accent hover:underline"
+					>
+						threaded composer pull request
+					</a>{' '}
+					so here it is my beautiful creation
 				</p>
 
 				<p class="mt-6 text-sm">
